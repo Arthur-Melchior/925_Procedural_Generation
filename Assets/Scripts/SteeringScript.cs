90b8f39098c7af1f4c768babe3ce65392c84cdd8
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2026-05-06 19:19:17 +0200

    added paause menu

--- Assets/Scripts/SteeringScript.cs
+++ Assets/Scripts/SteeringScript.cs
@@ -52,23 +52,97 @@ public class SteeringScript : MonoBehaviour
     private void AvoidCollisions()
     {
         var ahead = velocity * avoidDistance;
+        var angle =
+            Mathf.Atan2(velocity.x, velocity.y)
+            * Mathf.Rad2Deg;
+        var test = Physics2D.BoxCast(transform.position, ahead, angle, velocity, 1);
+        DrawBoxCast(transform.position, ahead, angle, velocity, 1, Color.bisque);
+        if (test)
+        {
+            Debug.Log(test.point);
+            Debug.Log(test.centroid);
+        }
+
         var collision = Physics2D.Raycast(transform.position, ahead, ahead.magnitude, layersToAvoid);
 
         Debug.DrawRay(transform.position, ahead, Color.aquamarine);
 
         if (collision)
         {
-            var avoidanceVector = Avoid(collision.centroid, collision.collider.bounds.center);
+            var avoidanceVector = Avoid(collision.point, collision.collider.bounds.center);
 
-            Debug.DrawRay((Vector2) transform.position + ahead, avoidanceVector, Color.red);
-            Debug.DrawRay(collision.centroid, ahead, Color.blue);
+            Debug.DrawRay(collision.collider.bounds.center, (Vector3)collision.point - collision.collider.bounds.center,
+                Color.blue);
 
             velocity += avoidanceVector;
+            Debug.DrawRay(transform.position, avoidanceVector, Color.red);
             Debug.DrawRay(transform.position, velocity);
         }
     }
 
+    void DrawBoxCast(
+        Vector2 origin,
+        Vector2 size,
+        float angle,
+        Vector2 direction,
+        float distance,
+        Color color)
+    {
+        Vector2 endPosition = origin + direction.normalized * distance;
+
+        DrawRotatedRectangle(origin, size, angle, color);
+        DrawRotatedRectangle(endPosition, size, angle, color);
+
+        Vector2[] startCorners = GetRectangleCorners(origin, size, angle);
+        Vector2[] endCorners = GetRectangleCorners(endPosition, size, angle);
+
+        for (int i = 0; i < 4; i++)
+        {
+            Debug.DrawLine(startCorners[i], endCorners[i], color);
+        }
+    }
+
+    void DrawRotatedRectangle(
+        Vector2 center,
+        Vector2 size,
+        float angle,
+        Color color)
+    {
+        Vector2[] corners = GetRectangleCorners(center, size, angle);
+
+        Debug.DrawLine(corners[0], corners[1], color);
+        Debug.DrawLine(corners[1], corners[2], color);
+        Debug.DrawLine(corners[2], corners[3], color);
+        Debug.DrawLine(corners[3], corners[0], color);
+    }
+
+    Vector2[] GetRectangleCorners(
+        Vector2 center,
+        Vector2 size,
+        float angle)
+    {
+        Vector2 half = size / 2f;
+
+        Vector2[] corners = new Vector2[4];
+
+        corners[0] = new Vector2(-half.x, half.y);   // TL
+        corners[1] = new Vector2(half.x, half.y);    // TR
+        corners[2] = new Vector2(half.x, -half.y);  // BR
+        corners[3] = new Vector2(-half.x, -half.y); // BL
+
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+
+        for (int i = 0; i < corners.Length; i++)
+        {
+            corners[i] = center + (Vector2)(rotation * corners[i]);
+        }
+
+        return corners;
+    }
+
     public Vector2 Seek(Transform seekTarget) => seekTarget.position - transform.position;
     public Vector2 Flee(Transform seekTarget) => transform.position - seekTarget.position;
-    public Vector2 Avoid(Vector2 velocity, Vector2 obstacle) => (velocity - obstacle) * avoidanceForce;
+
+    public Vector2 Avoid(Vector2 contactPoint, Vector2 obstacleCenter) =>
+        (contactPoint - obstacleCenter) * avoidanceForce;
 }
